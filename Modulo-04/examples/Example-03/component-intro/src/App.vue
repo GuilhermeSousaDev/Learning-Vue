fc202509d6913ba16a363ccf801cdc21888a8edd
Guilherme Augusto <a.guilhermerocha250@gmail.com>
2022-05-24 21:01:08 -0300

    Components CLI

---
+++ Modulo-04/examples/Example-03/component-intro/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h1>{{ count }}</h1>
+    <button @click="count++">Increment</button>
+    <button @click="count--">Decrement</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        count: 0
+      },
+    }
+  }
+</script>
+
+<style>
+
+</style>
