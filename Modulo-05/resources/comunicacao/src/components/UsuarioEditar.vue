6ba93b96f227b6e93199d4ce6dd1c25a36ef7adf
Guilherme Augusto <a.guilhermerocha250@gmail.com>
2022-06-05 17:15:28 -0300

    Manipulate Prop

--- Modulo-05/resources/comunicacao/src/components/UsuarioEditar.vue
+++ Modulo-05/resources/comunicacao/src/components/UsuarioEditar.vue
@@ -2,13 +2,23 @@
     <div class="componente">
         <h2>Alterar os Dados de Usuário</h2>
         <p>Edite as informações</p>
-        <p>Nome do Usuário: <strong>{{ name }}</strong></p>
+        <p>Nome do Usuário: <strong>{{ reverseName() }}</strong></p>
     </div>
 </template>
 
 <script>
 export default {
-    props: ['name']
+    props: ['name'],
+    data() {
+        return {
+            otherName: this.name,
+        }
+    },
+    methods: {
+        reverseName () {
+            return this.name.split('').reverse().join('')
+        }
+    },
 }
 </script>
 
