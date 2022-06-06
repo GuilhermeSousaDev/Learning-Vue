9d5fd464aaa0c196d3ec680047e559c81aed4119
Guilherme Augusto <a.guilhermerocha250@gmail.com>
2022-06-06 20:48:57 -0300

    Validate Props

---
+++ Modulo-05/examples/components/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Usuario from './components/Usuario.vue'
+</script>
+
+<template>
+  <button @click="changeName()">Change</button>
+  <Usuario />
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        name: 'Pedro'
+      }
+    },
+    methods: {
+      changeName () {
+        this.name = 'Ana';
+      }
+    },
+  }
+</script>
---
+++ Modulo-05/examples/components/src/components/Usuario.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h2>{{ name }}</h2>
+        <button @click="reverseName()">Reverse</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            name: {
+                type: String,
+                //required: true,
+                default: "Anonimo"
+            },
+        },
+        methods: {
+            reverseName() {
+                return this.name.split('').reverse('').join('')
+            }
+        }
+    }
+</script>
