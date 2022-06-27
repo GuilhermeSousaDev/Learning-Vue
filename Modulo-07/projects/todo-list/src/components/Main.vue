87929e0f4a54f713ba2f65d9a4dcb4bd13837b9b
Guilherme Augusto <a.guilhermerocha250@gmail.com>
2022-06-27 18:54:59 -0300

    Refactoring Challenge TodoList #2

---
+++ Modulo-07/projects/todo-list/src/components/Main.vue
@@ -0,0 +1,134 @@
+<script>
+import TodoList from './TodoList.vue';
+import TodoListManagment from './TodoListManagment.vue';
+
+export default {
+    created() {
+        this._loadValueInStorage();
+    },
+    data() {
+        return {
+            range: 0,
+            inpValue: "",
+            todoList: []
+        };
+    },
+    methods: {
+        addTodo() {
+            if (this.inpValue) {
+                const existedTodo = this.todoList.filter(todos => todos.text === this.inpValue);
+                if (!existedTodo.length) {
+                    const todo = {
+                        id: this.todoList.length + 1 || 1,
+                        text: this.inpValue,
+                        selected: false
+                    };
+                    this.todoList.push(todo);
+                    this.inpValue = "";
+
+                    this._setOrUpdateValueInStorage();
+                    this._setProgress();
+                }
+            }
+        },
+        removeTodo(id) {
+            this.todoList = this.todoList.filter(todo => todo.id !== id);
+
+            this._setOrUpdateValueInStorage();
+            this._setProgress();
+        },
+        selectTodo(id) {
+            const todo = this.todoList.filter(todo => todo.id === id)[0]; 
+            if(todo) todo.selected = !todo.selected;
+
+            this._setOrUpdateValueInStorage();
+            this._setProgress();
+        },
+        chooseTaskName(e) {
+            this.inpValue = e.target.value;
+        },  
+        _setOrUpdateValueInStorage() {
+            localStorage.setItem("todoList", JSON.stringify(this.todoList));
+        },
+        _loadValueInStorage() {
+            if (localStorage.getItem("todoList")) {
+                this.todoList = JSON.parse(localStorage.getItem("todoList"));
+                this._setProgress();
+            }
+        },
+        _setProgress() {
+            const selectedItems = this.todoList.filter(todo => todo.selected === true);
+            
+            if(!isNaN(Math.round(selectedItems.length * 100 / this.todoList.length))) {
+                this.range = Math.round(selectedItems.length * 100 / this.todoList.length);
+            } else {
+                this.range = 0;
+            }
+        },
+    },
+    components: { TodoList, TodoListManagment }
+}
+</script>
+
+<template>
+  <TodoListManagment
+    :range="range"
+    :inpValue="inpValue"
+    :addTodo="addTodo"
+    :chooseTaskName="chooseTaskName"
+  />
+
+  <TodoList 
+    :todoList="todoList" 
+    :removeTodo="removeTodo"
+    :selectTodo="selectTodo"
+  />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.todoList {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  width: 400px;
+  margin-top: 50px;
+}
+
+.todo {
+  border: 2px solid black;
+  border-radius: 5px;
+  margin-left: 10px;
+  padding: 5px;
+  margin-top: 10px;
+}
+
+.selected {
+  background: green;
+}
+
+#removeTodo {
+  width: 5px;
+  height: 5px;
+  border: 1px solid black;
+  padding: 2px;
+  cursor: pointer;
+}
+
+#removeTodo:hover {
+  color: white;
+  background: black;
+}
+</style>
---
+++ Modulo-07/projects/todo-list/src/components/TodoListManagment.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>Todo list</h1>
+        {{ range }}% <br>
+        <progress v-if="range" max="100" :value="range"></progress>
+        <br>
+        <input type="text" v-bind:value="inpValue" @input="e => chooseTaskName(e)" />
+        <br>
+        <button @click="addTodo()">Add Todo</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            range: {
+                type: Number,
+                required: true,
+                default: 0,
+            },
+            inpValue: String,
+            addTodo: Function,
+            chooseTaskName: Function
+        }
+    }
+</script>
