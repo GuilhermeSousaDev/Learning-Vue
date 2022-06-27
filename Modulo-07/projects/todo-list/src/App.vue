cd206769ea1165ac993198159fe5a35287d21459
Guilherme Augusto <a.guilhermerocha250@gmail.com>
2022-06-26 21:07:32 -0300

    Refactoring Challenge TodoList

--- Modulo-07/projects/todo-list/src/App.vue
+++ Modulo-07/projects/todo-list/src/App.vue
@@ -1,80 +1,82 @@
 <script>
-  export default {
+import TodoList from './components/TodoList.vue';
+
+export default {
     created() {
-      if (localStorage.getItem('todoList')) {
-        this.todoList = JSON.parse(localStorage.getItem('todoList'));
-        this._setProgress();
-      }
+        this._loadValueInStorage();
     },
     updated() {
-      this._setProgress();
+        this._setProgress();
     },
     data() {
-      return {
-        range: 0,
-        inpValue: '',
-        todoList: []
-      }
+        return {
+            range: 0,
+            inpValue: "",
+            todoList: []
+        };
     },
     methods: {
-      addTodo() {
-        if (this.inpValue) {
-          const todo = {
-            id: this.todoList.length + 1 || 1,
-            text: this.inpValue,
-            selected: false
-          }
-          this.todoList.push(todo);
-          this.inpValue = '';
+        addTodo() {
+            if (this.inpValue) {
+                const existedTodo = this.todoList.filter(todos => todos.text === this.inpValue);
+                if (!existedTodo.length) {
+                    const todo = {
+                        id: this.todoList.length + 1 || 1,
+                        text: this.inpValue,
+                        selected: false
+                    };
+                    this.todoList.push(todo);
+                    this.inpValue = "";
 
-          this._setOrUpdateValueInStorage();
-        }
-      },
-      removeTodo(id) {
-        this.todoList = this.todoList.filter(todo => todo.id !== id);
+                    this._setOrUpdateValueInStorage();
+                }
+            }
+        },
+        removeTodo(id) {
+            this.todoList = this.todoList.filter(todo => todo.id !== id);
 
-        this._setOrUpdateValueInStorage();
-      },
-      selectTodo(id) {
-        const todo = this.todoList.filter(todo => todo.id === id)[0];
-        todo.selected = !todo.selected;
+            this._setOrUpdateValueInStorage();
+            this._setProgress();
+        },
+        selectTodo(id) {
+            const todo = this.todoList.filter(todo => todo.id === id)[0];
+            todo.selected = !todo.selected;
 
-        this._setOrUpdateValueInStorage();
-      },
-      _setProgress() {  
-        const selectedItems = this.todoList.filter(todo => todo.selected === true);
-        this.range = Math.round(selectedItems.length * 100 / this.todoList.length);
-      },
-      _setOrUpdateValueInStorage() {
-        localStorage.setItem('todoList', JSON.stringify(this.todoList));
-      }
+            this._setOrUpdateValueInStorage();
+            this._setProgress();
+        },
+        _setOrUpdateValueInStorage() {
+            localStorage.setItem("todoList", JSON.stringify(this.todoList));
+        },
+        _loadValueInStorage() {
+            if (localStorage.getItem("todoList")) {
+                this.todoList = JSON.parse(localStorage.getItem("todoList"));
+                this._setProgress();
+            }
+        },
+        _setProgress() {
+            const selectedItems = this.todoList.filter(todo => todo.selected === true);
+            this.range = Math.round(selectedItems.length * 100 / this.todoList.length);
+        },
     },
-  }
+    components: { TodoList }
+}
 </script>
 
 <template>
   <h1>Todo list</h1>
   {{ range }}% <br>
-  <progress v-if="todoList.length && range" max="100" :value="range"></progress> 
+  <progress v-if="range" max="100" :value="range"></progress>
   <br>
   <input type="text" v-bind:value="inpValue" @input="e => inpValue = e.target.value" />
   <br>
   <button @click="addTodo()">Add Todo</button>
 
-  <div class="todoList" v-if="todoList.length">
-    <template v-for="todo in todoList">
-      <div v-if="todo.selected === true">
-        <div class="todo selected" @click="selectTodo(todo.id)">
-          <span @click="removeTodo(todo.id)" id="removeTodo">x</span>
-          <p>{{ todo.text }}</p>
-        </div>
-      </div>
-      <div v-else class="todo" @click="selectTodo(todo.id)">
-        <span @click="removeTodo(todo.id)" id="removeTodo">x</span>
-        <p>{{ todo.text }}</p>
-      </div>
-    </template>
-  </div>
+  <TodoList 
+    :todoList="todoList" 
+    :removeTodo="removeTodo"
+    :selectTodo="selectTodo"
+  />
 </template>
 
 <style>
@@ -90,6 +92,7 @@
   color: #2c3e50;
   margin-top: 60px;
 }
+
 .todoList {
   display: flex;
   justify-content: center;
@@ -97,6 +100,7 @@
   width: 400px;
   margin-top: 50px;
 }
+
 .todo {
   border: 2px solid black;
   border-radius: 5px;
@@ -104,9 +108,11 @@
   padding: 5px;
   margin-top: 10px;
 }
+
 .selected {
   background: green;
 }
+
 #removeTodo {
   width: 5px;
   height: 5px;
@@ -114,6 +120,7 @@
   padding: 2px;
   cursor: pointer;
 }
+
 #removeTodo:hover {
   color: white;
   background: black;
---
+++ Modulo-07/projects/todo-list/src/components/TodoList.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="todoList" v-if="todoList.length">
+        <template v-for="todo in todoList">
+            <div v-if="todo.selected === true">
+                <div class="todo selected" @click="selectTodo(todo.id)">
+                    <span @click="removeTodo(todo.id)" id="removeTodo">x</span>
+                    <p>{{ todo.text }}</p>
+                </div>
+            </div>
+            <div v-else class="todo" @click="selectTodo(todo.id)">
+                <span @click="removeTodo(todo.id)" id="removeTodo">x</span>
+                <p>{{ todo.text }}</p>
+            </div>
+        </template>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        todoList: {
+            type: Array,
+            required: true,
+        },
+        removeTodo: {
+            type: Function,
+            required: true,
+        },
+        selectTodo: {
+            type: Function,
+            required: true,
+        }
+    },
+}
+</script>
