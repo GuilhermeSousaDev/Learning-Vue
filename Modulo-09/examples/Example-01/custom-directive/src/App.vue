e1514b2484812a8c97283bb4ec169246e00e36a0
Guilherme Augusto <a.guilhermerocha250@gmail.com>
2022-07-13 20:41:16 -0300

    multiples modifiers in directive

--- Modulo-09/examples/Example-01/custom-directive/src/App.vue
+++ Modulo-09/examples/Example-01/custom-directive/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <p v-color="'blue'">Custom Blue Directive</p>
-    <p v-color.slow="color">Custom Red Directive</p>
-    <p v-color:background.slow="color">Custom arg Red Directive</p>
+    <p v-color.slow.alternate="color">Custom Red Directive</p>
+    <p v-color:background.slow.alternate="color">Custom arg Red Directive</p>
   </div>
 </template>
 
@@ -10,6 +10,38 @@
 
 export default {
   name: 'App',
+  directives: {
+    'color': {
+      bind(el, binding) {
+        const applyColor = color => {
+          if (binding.arg === 'background') {
+            el.style.backgroundColor = color;
+          } else {
+            el.style.color = color;
+          }
+        }
+
+        let slowing = 0;
+
+        if (binding.modifiers['slow']) slowing = 3000;
+
+        const colorOne = binding.value;
+        const colorTwo = 'purple';
+        let primaryColor = colorOne;
+
+        setTimeout(() => {
+          if (binding.modifiers['alternate']) {
+            setInterval(() => {
+              primaryColor = primaryColor === colorOne ? colorTwo : colorOne;
+              applyColor(primaryColor);
+            }, 1000); 
+          } else {
+            applyColor(binding.value);
+          }
+        }, slowing);
+      }
+    }
+  },
   data() {
     return {
       color: 'red'
--- Modulo-09/examples/Example-01/custom-directive/src/main.js
+++ Modulo-09/examples/Example-01/custom-directive/src/main.js
@@ -3,22 +3,6 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
-Vue.directive('color', {
-  bind(el, binding) {
-    let slowing = 0;
-
-    if (binding.modifiers['slow']) slowing = 3000;
-
-    setTimeout(() => {
-      if (binding.arg === 'background') {
-        el.style.backgroundColor = binding.value;
-      } else {
-        el.style.color = binding.value;
-      }
-    }, slowing);
-  }
-});
-
 new Vue({
   render: h => h(App),
 }).$mount('#app')
