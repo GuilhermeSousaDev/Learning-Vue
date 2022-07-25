92a6814a095d35118b96c7d5681b14c92f63f573
Guilherme Augusto <a.guilhermerocha250@gmail.com>
2022-07-25 20:29:19 -0300

    Init Project

---
+++ Modulo-11/examples/example-01/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <h1>Animações</h1>
+    <hr>
+    <button>Show Message</button>
+    <div>
+      <p>{{ msg }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      msg: 'Info User Message',
+      show: false
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
