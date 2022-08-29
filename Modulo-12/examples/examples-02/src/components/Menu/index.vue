5ecc77511841a6819468318ec49a471881f6efc5
Guilherme Augusto <a.guilhermerocha250@gmail.com>
2022-08-28 21:19:40 -0300

    Active Link

--- Modulo-12/examples/examples-02/src/components/Menu/index.vue
+++ Modulo-12/examples/examples-02/src/components/Menu/index.vue
@@ -1,9 +1,14 @@
 <template>
     <div>
-        <h1>Menu</h1>
-
-        <router-link to="/">Home</router-link>
-        <router-link to="/user">User</router-link>
+        <ul>
+            <h1>Menu</h1>
+            <router-link to="/" tag="li" active-class="active" exact>
+                <a>Home</a>
+            </router-link>
+            <router-link to="/user" tag="li" active-class="active">
+                <a>User</a>
+            </router-link>
+        </ul>
     </div>
 </template>
 
@@ -14,7 +19,25 @@
 </script>
 
 <style>
+    ul {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        background-color: black;
+        color: white;
+        padding: 0.5rem;
+    }
+    li {
+        list-style-type: none;
+        margin-left: 30px;
+    }
     a {
-        margin-left: 50px
+        text-decoration: none;
+    }
+    .active {
+        color: black;
+        background: white;
+        padding: 0.3rem;
+        border-radius: 5px;
     }
 </style>
