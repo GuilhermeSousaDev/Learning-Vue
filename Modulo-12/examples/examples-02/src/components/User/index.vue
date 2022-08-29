cd7610acb854fa2c90b0db94f2d4f1bbcc76536e
Guilherme Augusto <a.guilhermerocha250@gmail.com>
2022-08-29 20:51:55 -0300

    Get Params in URL Route

--- Modulo-12/examples/examples-02/src/components/User/index.vue
+++ Modulo-12/examples/examples-02/src/components/User/index.vue
@@ -1,11 +1,24 @@
 <template>
     <div>
         <h1>User Page</h1>
+
+        <p>ID: <strong>{{ id }}</strong></p>
+        <button @click="backToHome()">Voltar</button>
     </div>
 </template>
 
 <script>
     export default {
-        name: 'User'
+        name: 'User',
+        data() {
+            return {
+                id: this.$route.params.id,
+            }
+        },
+        methods: {
+            backToHome() {
+                this.$router.push('/');
+            }
+        }
     }
 </script>
--- Modulo-12/examples/examples-02/src/plugins/router.js
+++ Modulo-12/examples/examples-02/src/plugins/router.js
@@ -11,7 +11,7 @@ const routes = [
         component: Home,
     },
     {
-        path: '/user',
+        path: '/user/:id',
         component: User,
     },
 ];
