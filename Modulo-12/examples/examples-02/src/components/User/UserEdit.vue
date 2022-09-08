f262772fc0e2ceece2ca1e37aa9d84cbbb95d816
Guilherme Augusto <a.guilhermerocha250@gmail.com>
2022-09-08 20:17:57 -0300

    BeforeRouteLeave method

--- Modulo-12/examples/examples-02/src/components/User/UserEdit.vue
+++ Modulo-12/examples/examples-02/src/components/User/UserEdit.vue
@@ -6,7 +6,7 @@
         <p><strong>Complet: </strong> {{ $route.query.completed? 'Yes' : 'No' }} </p>
         <p><strong>Language</strong> {{ $route.query.lang }} </p> 
         <hr>
-        <button>Confirmar</button>
+        <button @click="confirm = true">Confirmar</button>
         <div class="rodape">
             <h1>Rodapé</h1>
         </div>
@@ -16,7 +16,23 @@
 <script>
     export default {
         name: 'UserEdit',
-        props: ['id']
+        props: ['id'],
+        data() {
+            return {
+                confirm: false
+            }
+        },
+        beforeRouteLeave(to, from, next) {
+            if (this.confirm) {
+                next();
+            } else {
+                if (confirm('Tem Certeza?')) {
+                    next();
+                } else {
+                    next(false);
+                }
+            }
+        }
     }
 </script>
 
