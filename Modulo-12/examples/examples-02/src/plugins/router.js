1ffc5149121526a808cbd693eaba3d95dd1596f4
Guilherme Augusto <a.guilhermerocha250@gmail.com>
2022-09-10 18:38:09 -0300

    Lazy load Route with function import

--- Modulo-12/examples/examples-02/src/plugins/router.js
+++ Modulo-12/examples/examples-02/src/plugins/router.js
@@ -2,12 +2,13 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../components/Home';
 import Menu from '../components/Menu';
-import User from '../components/User';
-import UserList from '../components/User/UserList';
-import UserDetails from '../components/User/UserDetails';
-import UserEdit from '../components/User/UserEdit';
 
 Vue.use(VueRouter);
+
+const User = () => import('../components/User');
+const UserEdit = () => import('../components/User/UserEdit');
+const UserList = () => import('../components/User/UserList');
+const UserDetails = () => import('../components/User/UserDetails');
 
 const routes = [
     {
