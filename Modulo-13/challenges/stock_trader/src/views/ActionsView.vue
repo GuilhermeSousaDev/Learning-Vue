c7b866b054b72c3e80af8c765d46d5c6edb7bb43
Guilherme Augusto <a.guilhermerocha250@gmail.com>
2022-09-25 09:28:15 -0300

    ActionsView

---
+++ Modulo-13/challenges/stock_trader/src/views/ActionsView.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="actions">
+        <div v-for="action in actions" class="action">
+            <div class="action_info">
+                <span>{{ action.name }}</span> <span>( {{ action.price }} )</span>
+                <div class="buy_action">
+                    <input type="number" v-model="quantity_actions" />
+                    <button @click="buyAction()">Buy</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ActionView',
+    data() {
+        return {
+            quantity_actions: 0,
+        }
+    },
+    methods: {
+        buyAction() {
+            console.log(quantity_actions);
+        }
+    },
+    computed: {
+        actions() {
+            return this.$store.state.actions;
+        }
+    }
+}
+</script>
