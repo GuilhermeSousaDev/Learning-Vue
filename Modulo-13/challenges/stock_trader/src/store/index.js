50ad178beef1b74288ea69161c6c0997c2a50e1e
Guilherme Augusto <a.guilhermerocha250@gmail.com>
2022-09-25 21:36:49 -0300

    Create BuyAction Mutation

--- Modulo-13/challenges/stock_trader/src/store/index.js
+++ Modulo-13/challenges/stock_trader/src/store/index.js
@@ -8,18 +8,22 @@ export default new Vuex.Store({
     cash: 10000,
     actions: [
       {
+        id: 1,
         name: 'BMW',
         price: 110,
       },
       {
+        id: 2,
         name: 'Google',
         price: 200,
       },
       {
+        id: 3,
         name: 'Apple',
         price: 250,
       },
       {
+        id: 4,
         name: 'Twitter',
         price: 12,
       }
@@ -29,6 +33,9 @@ export default new Vuex.Store({
   getters: {
   },
   mutations: {
+    buyAction(state, payload) {
+      state.actions.push(payload);
+    }
   },
   actions: {
   },
---
+++ Modulo-13/challenges/stock_trader/src/views/PortfolioView.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="actions">
+        <div v-for="action in actions" class="action">
+            <div class="action_info">
+                <span>{{ action.name }}</span> <span>( {{ action.price }} )</span>
+                <div class="buy_action">
+                    <input type="number" v-model="quantity_actions" />
+                    <button @click="sellAction()">Sell</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PortfolioView',
+    data() {
+        return {
+            quantity_actions: 0,
+        }
+    },
+    methods: {
+        sellAction() {
+            console.log(quantity_actions);
+        }
+    },
+    computed: {
+        actions() {
+            return this.$store.state.bought_actions;
+        }
+    }
+}
+</script>
