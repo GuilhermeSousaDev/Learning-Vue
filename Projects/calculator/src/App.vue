5f9128e69368cf2b337884bad8b19ca8981327e5
Guilherme Augusto <a.guilhermerocha250@gmail.com>
2022-10-31 17:25:44 -0300

    Components Button, Display and Calculator

---
+++ Projects/calculator/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <h1>Calculator</h1>
+    <Calculator />
+  </div>
+</template>
+
+<script>
+import Calculator from './main/Calculator.vue';
+
+export default {
+  name: 'App',
+  components: { Calculator }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Projects/calculator/src/main/Calculator.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>0</div>
+</template>
+
+<script>
+import Button from '../components/Button.vue';
+import Display from '../components/Display.vue';
+
+export default {
+    name: 'Calculator',
+    components: { Button, Display }
+}
+</script>
+
+<style>
+
+</style>
